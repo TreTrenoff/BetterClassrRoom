630ef6a19fc871647e8013f00ae882508a9ba5f5
Trenoff <trenoff.tro@gmail.com>
2026-02-04 12:20:58 +0100

    ajout login non fonctionnelle (pb  de root je ne sais pas comment faire)

--- urls.py
+++ urls.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+def login(request):
+    return render(request, "login.html")
+
+
 
 urlpatterns = [
     path("", index),
+    path("/login", login),
 ]
